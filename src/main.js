971fcb31cb3179dae5a308d30c04baf928d50ac4
Guest User <Guest@epicodus-94.local>
2018-08-10 13:55:15 -0700

    removed find by keyword function

--- src/main.js
+++ src/main.js
@@ -10,20 +10,27 @@ import { Doctor } from './doctor.js';
 $(document).ready(function() {
   $('#bykeyWord').click(function() {
     let input = $('#userInput').val();
-    $("userInput").val("");
+    $("#userInput").val("");
 
 
     let newDoctor = new Doctor();
     let promise = newDoctor.findDoctorByKey(input);
+    
 
-    promise.then(function(response) {
-      let doctor = JSON.parse(response);
-      
-      for (let i = 0; i < doctor.data.length; i++) {
-        $("#output").append("<div>" +
-      '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>'
-    );
-  }
+    promise.then(function (response) {
+     let doctor = JSON.parse(response);
+     for(let i = 0; i < doctor.data.length; i++){
+       $("#output").prepend("<div>" +
+        '<h2>' + doctor.data[i].profile.first_name + " " + doctor.data[i].profile.last_name + '</h2>' +
+        '<p>' + doctor.data[i].practices[0].phones[0].number + '</p>' +
+        '<p>' + doctor.data[i].practices[0].visit_address.street + '</p>' +
+        '<p>' + doctor.data[i].practices[0].visit_address.city + "," +
+        doctor.data[i].practices[0].visit_address.state +  " " +
+        doctor.data[i].practices[0].visit_address.zip +
+        '</p>' +
+        "</div>"
+       );
+     }
 }, function (error) {
   $("#error").text(`there was an Error:${error.message}`);
       $("#error").text(`there was an Error:${error.message}`);
